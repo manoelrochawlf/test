96090df4851eface74279f12cef2a8ac8847f7ff
Manoel Rocha <manoelrocha399@gmail.com>
2023-08-02 13:40:26 -0300

    feat: done simple_calculator

---
+++ simple_calculator.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function calcular(numero1, operador, numero2) {
+  switch (operador) {
+    case '+':
+      return numero1 + numero2;
+    case '-':
+      return numero1 - numero2;
+    case '*':
+      return numero1 * numero2;
+    case '/':
+      if (numero2 !== 0) {
+        return numero1 / numero2;
+      } else {
+        return 'Erro: divisão por zero não é permitida!';
+      }
+    default:
+      return 'Operador inválido. Use +, -, * ou /.';
+  }
+}
+
+rl.question('Digite o primeiro número: ', (numero1) => {
+  rl.question('Digite o operador (+, -, *, /): ', (operador) => {
+    rl.question('Digite o segundo número: ', (numero2) => {
+      const resultado = calcular(parseFloat(numero1), operador, parseFloat(numero2));
+      console.log(`O resultado é: ${resultado}`);
+      rl.close();
+    });
+  });
+});
